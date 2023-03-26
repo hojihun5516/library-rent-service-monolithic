1ccecd8789cacdf4fde27ca70e06d3d33634b9b3
modernflow <hojihun5516@daum.net>
2023-03-26 14:50:58 +0900

    init project

---
+++ src/main/kotlin/com/example/libraryrental/LibraryRentalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.libraryrental
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryRentalApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryRentalApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/libraryrental/LibraryRentalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.libraryrental
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryRentalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
